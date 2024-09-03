be0ff4c8bbcabdab00831ee4439931e7566d04b7
Marina <marisham1223@gmail.com>
2024-09-03 13:01:34 +0300

    user ava

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,12 +20,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $faker=Faker::create();
-        $originalAvatarPath = public_path('avatars\user.png'); // Шлях до оригінального зображення
+
 
         // Отримуємо URL до збереженого зображення
-        $url = url(Storage::url('avatars\user.png'));
-
-//        $url = 'http://127.0.0.1:8000' . Storage::url('avatars/user.png');
+        $url = url('https://media.istockphoto.com/id/1361394182/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D0%B1%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BE%D1%80%D0%BE%D1%82%D0%BA%D0%BE%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%BA%D0%BE%D1%88%D0%BA%D0%B8-%D0%B2%D1%8B%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F%D1%89%D0%B8%D0%B9-%D1%88%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC-%D0%B8%D0%BB%D0%B8-%D1%83%D0%B4%D0%B8%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC.jpg?s=612x612&w=0&k=20&c=yEEyxvdyb-jUxnHmr8nXSf9qQPS0WfkhBITLVIaj7OY=');
 
         for ($i=0;$i<45;$i++){
             User::create([
