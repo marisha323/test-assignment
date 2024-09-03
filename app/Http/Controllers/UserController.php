4c56beca839eab001b28d0fd77a05257c4ad3ab7
Marina <marisham1223@gmail.com>
2024-09-03 12:00:48 +0300

    user ava

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,48 +35,57 @@ class UserController extends Controller
         }
 
 
-        $avatarPath = null;
+        $filePath = storage_path('app/public/uploads/' . $request->filename);
 
-        if ($request->hasFile('avatar')) {
-            $file = $request->file('avatar');
-
-            $path = $file->store('uploads', 'public');
-            $fullPath = Storage::path($path);
-            // Використання бібліотеки Tinify для оптимізації зображення
-            \Tinify\setKey("YQq20x4f4RfWLdHbfvCKLWbQ489b591r");
-
-
-                $source = \Tinify\fromFile($path);
-
-
-                $resized = $source->resize([
-                    "method" => "fit",
-                    "width" => 70,
-                    "height" => 70
-                ]);
-
-            // Отримання імені файлу без розширення
-            $filenameWithoutExtension = pathinfo($path, PATHINFO_FILENAME);
-
-            // Збереження оптимізованого зображення
-            $resizedPath = 'uploads/' . $filenameWithoutExtension . '_thumb.jpg';
-            $resized->toFile(Storage::path($resizedPath));
-
-            // Шлях, який буде збережено в базі даних
-                $avatarPath = $resizedPath;
-
+        if (file_exists($filePath)) {
+            return response()->json(['message' => 'File exists'], 200);
+        } else {
+            return response()->json(['message' => 'File not found'], 404);
         }
 
-        $url = url(Storage::url($avatarPath));
-//dd($url);
-        // Створення користувача
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'avatar' => $url,
-        ]);
-
-        return response()->json($user);
+//        $avatarPath = null;
+//
+//        if ($request->hasFile('avatar')) {
+//            $file = $request->file('avatar');
+//
+//            $path = $file->store('uploads', 'public');
+//            $fullPath = Storage::path($path);
+//            $fullUrl = url(Storage::url($avatarPath));
+//            // Використання бібліотеки Tinify для оптимізації зображення
+//            \Tinify\setKey("YQq20x4f4RfWLdHbfvCKLWbQ489b591r");
+//
+//
+//                $source = \Tinify\url($fullUrl);
+//
+//
+//                $resized = $source->resize([
+//                    "method" => "fit",
+//                    "width" => 70,
+//                    "height" => 70
+//                ]);
+//
+//            // Отримання імені файлу без розширення
+//            $filenameWithoutExtension = pathinfo($path, PATHINFO_FILENAME);
+//
+//            // Збереження оптимізованого зображення
+//            $resizedPath = 'uploads/' . $filenameWithoutExtension . '_thumb.jpg';
+//            $resized->toFile(Storage::path($resizedPath));
+//
+//            // Шлях, який буде збережено в базі даних
+//                $avatarPath = $resizedPath;
+//
+//        }
+//
+//        $url = url(Storage::url($avatarPath));
+////dd($url);
+//        // Створення користувача
+//        $user = User::create([
+//            'name' => $request->name,
+//            'email' => $request->email,
+//            'password' => bcrypt($request->password),
+//            'avatar' => $url,
+//        ]);
+//
+//        return response()->json($user);
     }
 }
